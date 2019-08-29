c9e6a9dc863cfc970ac945616a7687f1f4e85cb6
Christos Bampis <christosb@netflix.com>
2019-08-28 18:34:46 -0700

    Put back print progress for frame.

--- feature/src/common/frame.c
+++ feature/src/common/frame.c
@@ -126,7 +126,7 @@ int read_frame(float *ref_data, float *dis_data, float *temp_data, int stride_by
         goto fail_or_end;
     }
     
-    dbg_fprintf(stderr, "Frame: %d/%d\r", completed_frames++, user_data->num_frames);
+    fprintf(stderr, "Frame: %d/%d\r", completed_frames++, user_data->num_frames);
 
 fail_or_end:
     return ret;
